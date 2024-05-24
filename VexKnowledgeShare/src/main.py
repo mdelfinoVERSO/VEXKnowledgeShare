7937fb8f111f0e3095b57d37dae4637f4e5eecb5
Michael Delfino <michael.delfino@verso.ac.th>
2024-05-24 14:11:22 +0700

    Initial commit

---
+++ VexKnowledgeShare/src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       michael.delfino                                              #
+# 	Created:      5/24/2024, 2:09:51 PM                                        #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
